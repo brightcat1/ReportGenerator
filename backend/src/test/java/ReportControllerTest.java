c9c3db72ae5d602d02dcc2182949d033ce839c75
brightcat <bright11jp@yahoo.co.jp>
2023-09-18 16:49:20 +0900

    Added a backend test case

--- backend/src/test/java/ReportControllerTest.java
+++ backend/src/test/java/ReportControllerTest.java
@@ -1,9 +1,7 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import reportgenerator.controller.ReportController;
@@ -24,12 +22,23 @@ class ReportControllerTest {
     private ReportRenderer htmlRenderer;
 
     @Mock
+    private ReportRenderer textRenderer;
+
+    @Mock
     private ReportDownloader htmlDownloader;
 
+    @Mock
+    private ReportDownloader textDownloader;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+        reportController = new ReportController(htmlRenderer, textRenderer, htmlDownloader, textDownloader);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Mockito.reset(htmlRenderer, htmlDownloader, textRenderer, textDownloader);
     }
 
     @Test
@@ -53,5 +62,28 @@ class ReportControllerTest {
         assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("Test HTML Report", new String(response.getBody()));
+    }
+
+    @Test
+    void testDownloadReportAsText() {
+        ReportController.ReportRequest request = new ReportController.ReportRequest("Test Title", "Test SubTitle", "Test Content", "Test Footer");
+
+        byte[] mockReportData = "Test Text Report".getBytes();
+
+        ArgumentCaptor<Report> reportCaptor = ArgumentCaptor.forClass(Report.class);
+        when(textRenderer.render(reportCaptor.capture())).thenReturn(mockReportData);
+        when(textDownloader.downloadReport(mockReportData)).thenReturn(ResponseEntity.ok(mockReportData));
+
+        ResponseEntity<byte[]> response = reportController.downloadReportAsText(request);
+
+        Report capturedReport = reportCaptor.getValue();
+        assertEquals("Test Title", capturedReport.getTitle());
+        assertEquals("Test SubTitle", capturedReport.getSubTitle());
+        assertEquals("Test Content", capturedReport.getContent());
+        assertEquals("Test Footer", capturedReport.getFooter());
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Test Text Report", new String(response.getBody()));
     }
 }
